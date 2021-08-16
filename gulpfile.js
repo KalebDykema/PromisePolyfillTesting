639c4d52c3ffa1dba0cfa1e4701415a2377e4114
Kaleb Dykema <dykemakaleb@gmail.com>
2021-08-16 10:37:15 -0500

    Set up gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require ('gulp'),
+    babel = require('gulp-babel'),
+	concat = require('gulp-concat'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload
+
+// utilty function to get all folders in a directory
+const getFolders = function (dir) {
+	return fs.readdirSync(dir)
+		.filter(function (file) {
+			return fs.statSync(path.join(dir, file)).isDirectory();
+		});
+};
+
+const transpileJS = function(strData=false){
+	console.log(`Transpile JS: ${strData}`)
+	gulp.src([`src/js/*.js`])
+		.pipe(concat('main.js'))
+		.pipe(babel({
+			presets: ['@babel/preset-env']
+			// "presets": [
+			// 	["@babel/preset-env", {
+			// 		"targets": "last 2 versions, ie 11"
+			// 	}]
+			// ]
+		}))
+		.pipe(gulp.dest(`dist/js/`))
+	reload()
+}
+
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		server: {
+            baseDir: './dist'
+        }
+	});
+	transpileJS()
+	// https://gulpjs.com/docs/en/getting-started/explaining-globs/
+	gulp.watch(['dist/*.html']).on('change', reload);
+	gulp.watch(['src/js']).on('change', transpileJS);
+});
