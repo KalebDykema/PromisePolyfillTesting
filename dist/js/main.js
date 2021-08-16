1d63715f3ad8bc16a7ee0a6d4fa4eb86d6673c25
Kaleb Dykema <dykemakaleb@gmail.com>
2021-08-16 11:12:24 -0500

    Added fetch function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,19 @@
 "use strict";
 
 console.log('Hello world');
+
+var getProfile = function getProfile(username) {
+  return fetch("https://api.github.com/users/".concat(username)).then(function (data) {
+    console.log(data.json())({
+      name: data.name,
+      location: data.location,
+      company: data.company
+    });
+  })["catch"](function (e) {
+    console.warn(e);
+  });
+};
+
+getProfile('KalebDykema').then(function (response) {
+  console.log(response);
+});
--- src/js/promises.js
+++ src/js/promises.js
@@ -1,1 +1,21 @@
 console.log('Hello world')
+
+const getProfile = username => {
+    return fetch(`https://api.github.com/users/${username}`)
+        .then((data) => {
+            console.log(data.json())
+            ({
+                name: data.name,
+                location: data.location,
+                company: data.company
+            })
+        })
+        .catch(e => {
+            console.warn(e)
+        })
+}
+
+getProfile('KalebDykema')
+    .then(response => {
+        console.log(response)
+    })
